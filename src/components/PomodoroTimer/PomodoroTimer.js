99aecd3445a4bd45840deef6efb9798d907ff4b4
James Shirlaw <jamespshirlaw@gmail.com>
2018-02-04 14:36:59 -0800

    cleanup and async setState

--- src/components/PomodoroTimer/PomodoroTimer.js
+++ src/components/PomodoroTimer/PomodoroTimer.js
@@ -9,26 +9,42 @@ export default class PomodoroTimer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentSeconds: null,
       pomodoros: 0,
       started: false
     };
   }
   startTimer = () => {
-    let currentSeconds = secondsInTwentyFiveMinutes;
+    this.setState({ currentSeconds: secondsInTwentyFiveMinutes });
     if (this.timer) clearInterval(this.timer);
     this.timer = setInterval(() => {
-      if (currentSeconds >= 0) {
-        let minutes = Math.floor(currentSeconds / 60);
-        let seconds = currentSeconds - minutes * 60;
-        if (`${seconds}`.length == 1) seconds = `0${seconds}`;
-        this.setState({ seconds, minutes, started: true });
-        currentSeconds = currentSeconds - 1;
+      if (this.state.currentSeconds >= 0) {
+        this.incrementTimer();
       } else {
-        clearInterval(this.timer);
-        this.setState({ started: false, mintues: null, seconds: null });
+        this.resetTimer();
       }
     }, 1000);
   };
+
+  resetTimer() {
+    clearInterval(this.timer);
+    this.setState({ started: false, mintues: null, seconds: null });
+  }
+
+  incrementTimer() {
+    let { currentSeconds } = this.state;
+    let minutes = Math.floor(currentSeconds / 60);
+    let seconds = currentSeconds - minutes * 60;
+    if (`${seconds}`.length == 1) seconds = `0${seconds}`;
+
+    this.setState(prevState => ({
+      currentSeconds: prevState.currentSeconds - 1,
+      seconds,
+      minutes,
+      started: true
+    }));
+  }
+
   render() {
     let { minutes, seconds, started } = this.state;
     return (
