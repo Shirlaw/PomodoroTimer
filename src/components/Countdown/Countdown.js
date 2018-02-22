f1ddc1f2f37f9be9a6cc4a90e5877dc43bc13518
James Shirlaw <james_shirlaw@acl.com>
2018-02-22 09:10:15 -0800

    refactor countdown

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Component } from "react";
 
 export default class PomodoroTimer extends Component {
+  renderTime() {
+    const { seconds, minutes } = this.props;
+    return (
+      <div>
+        {minutes}:
+        {seconds}
+      </div>
+    );
+  }
   render() {
     const { seconds, minutes } = this.props;
-    if (minutes || seconds) {
-      return (
-        <div>
-          {minutes}:
-          {seconds}
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return minutes || seconds ? this.renderTime() : null;
   }
 }
