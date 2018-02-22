3cc1edc1ea586a996b676342e3f3632bb35ba9e5
James Shirlaw <james_shirlaw@acl.com>
2018-02-22 09:49:07 -0800

    add redux dev tool

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from "redux";
 import pomodoroApp from "./reducers/pomodoroApp";
 import PomodoroTimer from "./components/PomodoroTimer/PomodoroTimer";
 
-let store = createStore(pomodoroApp);
+let store = createStore(pomodoroApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
   <Provider store={store}>
